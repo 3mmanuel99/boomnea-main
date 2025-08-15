aee2c96069e0ef59124589c77355165c4dfc34e2
= <=>
2025-08-15 11:14:49 +0100

    test2

---
+++ src/lib/scraping.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+axios.get("https://en.wikipedia.org/wiki/Special:Random")
+    .then(function(response) {
+        const $ = cheerio.load(response.data);
+        if ($("table.wikitable:not(.navbox)").length > 0) {
+            console.log("This page has tables!");
+        }
+        else {
+            const paragraph = $("p")
+            .each(function() {
+                const text = $(this).text();
+                const cleanText = text.trim();
+
+                const regex = new RegExp("\\[\\d+\\]", "g")
+                let cleanerText = cleanText.replaceAll(regex, "")
+                console.log(cleanerText);
+            });
+        }
+});
